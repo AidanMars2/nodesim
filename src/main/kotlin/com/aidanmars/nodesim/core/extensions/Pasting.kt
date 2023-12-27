638eaba2ab9cd7666f3ed1b1afe824ed7d9113c8
AidanMars <aidanmars2008@gmail.com>
2023-12-27 15:28:40 +0100

    fix Pasting.kt null pointer

--- src/main/kotlin/com/aidanmars/nodesim/core/extensions/Pasting.kt
+++ src/main/kotlin/com/aidanmars/nodesim/core/extensions/Pasting.kt
@@ -4,16 +4,17 @@ import com.aidanmars.nodesim.core.Circuit
 import com.aidanmars.nodesim.core.Node
 
 fun Circuit.paste(
-    circuit: Circuit,
+    other: Circuit,
     xOffset: Int = 0, yOffset: Int = 0
 ) {
     val nodeMap = mutableMapOf<Node, Node>()
-    val otherNodes = circuit.nodes.values
+    val otherNodes = other.nodes.values
     otherNodes.forEach { node ->
         val newNode = createNode(node.x + xOffset, node.y + yOffset, node.type)
         newNode.outputPower = node.outputPower
         newNode.inputPower = node.inputPower
         newNode.inputPowerBuffer = node.inputPowerBuffer
+        nodeMap[node] = newNode
     }
 
     otherNodes.forEach { node ->
@@ -21,6 +22,6 @@ fun Circuit.paste(
         node.outputNodes.forEach {
             connectNodes(mirrorNode, nodeMap[it]!!, false, false)
         }
-        if (node in circuit.updates) updates.add(mirrorNode)
+        if (node in other.updates) updates.add(mirrorNode)
     }
 }
